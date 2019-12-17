97b5236e98ec8ac049ed4d379089ba7d75531e9c
David <mail@davidhinrichs.de>
2019-12-17 16:02:39 +0100

    try voila gridstack

--- docker-images/notebook-kubeflow/prepare.sh
+++ docker-images/notebook-kubeflow/prepare.sh
@@ -79,6 +79,6 @@ if [ "$GCSFUSE_BUCKET" ]; then
     /opt/conda/bin/gcsfuse $GCSFUSE_BUCKET /gcs --background
 fi
 
-jupyter lab --notebook-dir=/home/jovyan --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=$NB_PREFIX
+jupyter lab --notebook-dir=/home/jovyan --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=$NB_PREFIX --VoilaConfiguration.template=gridstack --VoilaConfiguration.resources='{"gridstack": {"show_handles": True}}'
 
 $@
