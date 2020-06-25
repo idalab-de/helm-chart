20e92364d9eb18f3585641c4dc24d77d69b2214d
felihong <echozhu1991@gmail.com>
2020-06-25 18:06:36 +0200

    Reset failed entrypoint settings.

--- docker-images/notebook-kubeflow/prepare.sh
+++ docker-images/notebook-kubeflow/prepare.sh
@@ -130,4 +130,6 @@ if [ -z "$DEVMODE" ]; then
 
 fi
 
+jupyter lab --notebook-dir=/home/jovyan --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=$NB_PREFIX --VoilaConfiguration.template=gridstack --VoilaConfiguration.resources='{"gridstack": {"show_handles": True}}'
+
 $@
